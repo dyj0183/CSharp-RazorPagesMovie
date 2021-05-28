ee01bd570478369f06a81cfbe4e9f87618c6f37d
Yu-Chun Tung <dyj0183@byui.edu>
2021-05-28 10:25:27 -0600

    Add project files.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; } // this is required for the primary key in the database
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)] // specifies the type of the data: (Date)
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+
+    }
+}
